4d1aebebe9b8952ff5bbca8b6540c49e5898b855
valya_plotnikova <valya_ezhova@mail.ru>
2024-07-17 14:19:19 +0300

    update admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from catalog.models import Product, Category
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    ist_display = ('ID', 'product_name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('product_name', 'description')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ist_display = ('ID', 'category_name')
+    search_fields = ('category_name', 'description')
