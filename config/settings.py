85c5360d01af72e3091d6e43adf66a3e3b4c5ba4
valya_plotnikova <valya_ezhova@mail.ru>
2024-07-23 12:26:46 +0300

    update setting

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIR = [
-    BASE_DIR / "static"
+    BASE_DIR / "static",
 ]
 
 MEDIA_URL = '/media/'
