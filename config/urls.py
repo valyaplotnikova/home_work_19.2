e5c362100c8fcb5254954bf7f87663e89300d6fa
valya_plotnikova <valya_ezhova@mail.ru>
2024-07-29 14:11:45 +0300

    update files

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls'))
+    path('', include('catalog.urls')),
+    path('blog/', include('blog.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
