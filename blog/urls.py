037054a9c3b0db5f53dbde48d1c9e167613189ef
valya_plotnikova <valya_ezhova@mail.ru>
2024-07-29 14:11:57 +0300

    create files

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from blog.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
+
+urlpatterns = [
+    path('list/', BlogListView.as_view(), name='blog_list'),
+    path('about/<int:pk>/', BlogDetailView.as_view(), name='about'),
+    path('create/', BlogCreateView.as_view(), name='create_blog'),
+    path('create/<int:pk>/', BlogUpdateView.as_view(), name='update_blog'),
+    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete_blog')
+]
