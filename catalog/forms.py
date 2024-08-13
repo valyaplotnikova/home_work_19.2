832d55dd1d5477a8dd26dbc7b4526aa76caef5fd
valya_plotnikova <valya_ezhova@mail.ru>
2024-08-13 20:07:30 +0300

    update forms product

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Category, Product, Contacts
+from django.core.exceptions import ValidationError
+from .models import Category, Product, Contacts, Version
 
 
 class AddProductForm(forms.ModelForm):
@@ -16,6 +17,22 @@ class AddProductForm(forms.ModelForm):
             'price': 'Цена книги'
         }
 
+    forbidden_words = ["казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"]
+
+    def clean_product_name(self):
+        cleaned_data = self.cleaned_data['product_name']
+        for forbidden_word in self.forbidden_words:
+            if forbidden_word in cleaned_data.lower():
+                raise ValidationError(f'Наименование не должно содержать слово "{forbidden_word}"')
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data['description']
+        for forbidden_word in self.forbidden_words:
+            if forbidden_word in cleaned_data.lower():
+                raise ValidationError(f'Описание не должно содержать слово "{forbidden_word}"')
+        return cleaned_data
+
 
 class AddContactForm(forms.ModelForm):
 
@@ -27,3 +44,9 @@ class AddContactForm(forms.ModelForm):
             'phone': forms.TextInput(attrs={'class': 'form-input'}),
             'massage': forms.Textarea(attrs={'cols': 30, 'rows': 5}),
         }
+
+
+class VersionForm(forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = ['product', 'version_number', 'version_name', 'is_version_active']
