a414a916be95c8c4e1a64498cd190d1f65ef0a16
valya_plotnikova <valya_ezhova@mail.ru>
2024-08-13 20:07:10 +0300

    update urls product

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,11 +2,14 @@
 from django.urls import path
 
 from catalog import views
-from catalog.views import ProductListView, ProductDetailView, ProductCreateView, AddContact
+from catalog.views import ProductListView, ProductDetailView, ProductCreateView, AddContact, ProductUpdateView, \
+    ProductDeleteView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('contacts/', AddContact.as_view(), name='contacts'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='about_product'),
-    path('place_a_product/', ProductCreateView.as_view(), name='place_a_product')
+    path('place_a_product/', ProductCreateView.as_view(), name='place_a_product'),
+    path('update_product/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
+    path('delete_product/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
 ]
