3c85552f48571f294eb3b2ae5997ee8274882974
valya_plotnikova <valya_ezhova@mail.ru>
2024-08-20 19:50:10 +0300

    create users files

---
+++ users/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+
+from users.models import User
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2',)
