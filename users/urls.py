b5e1cf24514c25df08b1a3f4fe12f7c43362f190
valya_plotnikova <valya_ezhova@mail.ru>
2024-08-22 15:56:46 +0300

    create reset_password

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView, email_verification
+from users.views import RegisterView, ProfileView, email_verification, reset_password
 
 app_name = UsersConfig.name
 
@@ -12,4 +12,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('email-confirm/<str:token>/', email_verification, name='email-confirm'),
+    path('reset_password/', reset_password, name='reset_password'),
+
     ]
